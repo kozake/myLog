5d090ccba79f08f7841948495f00ec49da4f4ae9
kozake <sh.kozake@gmail.com>
2015-11-12 08:32:33 +0900

    修正もれてた

--- www/js/index.js
+++ www/js/index.js
@@ -16,30 +16,6 @@
  * specific language governing permissions and limitations
  * under the License.
  */
- $(document).ready(function() {
-   $(".animsition").animsition({
-     inClass: 'fade-in',
-     outClass: 'fade-out',
-     inDuration: 1500,
-     outDuration: 800,
-     linkElement: '.animsition-link',
-     // e.g. linkElement: 'a:not([target="_blank"]):not([href^=#])'
-     loading: true,
-     loadingParentElement: 'body', //animsition wrapper element
-     loadingClass: 'animsition-loading',
-     loadingInner: '', // e.g '<img src="loading.svg" />'
-     timeout: false,
-     timeoutCountdown: 5000,
-     onLoadEvent: true,
-     browser: [ 'animation-duration', '-webkit-animation-duration'],
-     // "browser" option allows you to disable the "animsition" in case the css property in the array is not supported by your browser.
-     // The default setting is to disable the "animsition" in a browser that does not support "animation-duration".
-     overlay : false,
-     overlayClass : 'animsition-overlay-slide',
-     overlayParentElement : 'body',
-     transition: function(url){ window.location.href = url; }
-   });
- });
 var app = {
     // Application Constructor
     initialize: function() {
