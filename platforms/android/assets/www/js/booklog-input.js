555129c7ad440a1a8d208d397d86f85a689753c9
kozake <sh.kozake@gmail.com>
2015-11-18 08:25:37 +0900

    もう少しでモック終ろかな

--- platforms/android/assets/www/js/booklog-input.js
+++ platforms/android/assets/www/js/booklog-input.js
@@ -33,30 +33,35 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-      var chartdata74 = {
-
-        "config": {
-          "title": "シングルページWebアプリケーション",
-          "subTitle": "読み始めてから32日目です。",
-          "type": "line",
-          "useVal": "yes",
-          "useMarker": "css-ring",
-          "bg": "#fff",
-          "textColor": "#000",
-          "xColor": "rgba(100,100,100,0.3)",
-          "maxY": 483,
-          "minY": 0,
-          "lineWidth": "1",
-          "valYOffset": 10
-        },
-        "data": [
-          ["日","3/1","3/2","3/3","3/4","3/5","3/6","3/7"],
-          ["シングル~",30,43,50,64,65,65,78]
-        ]
-      };
-
-      ccchart.init('hoge', chartdata74);
     }
 };
 
 app.initialize();
+var chartdata74 = {
+
+  "config": {
+    "title": "シングルページWebアプリケーション",
+    "subTitle": "読み始めてから32日目です。",
+    "type": "line",
+    "useVal": "yes",
+    "useMarker": "arc",
+    "bg": "#fff",
+    "textColor": "#000",
+    "xColor": "rgba(100,100,100,0.3)",
+    "maxY": 483,
+    "minY": 0,
+    "lineWidth": "3",
+    "valYOffset": 10,
+    "markerWidth": 15
+  },
+  "data": [
+    ["日","3/1","3/2","3/3","3/4","3/5","3/6","3/7"],
+    ["シングル~",30,43,50,164,175,265,378]
+  ]
+};
+
+ccchart.init('hoge', chartdata74);
+
+$(function() {
+  $( "#datepicker" ).datepicker();
+});
