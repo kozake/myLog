0240fdaf4f09487e45e293fa3c4934783470b49d
kozake <sh.kozake@gmail.com>
2015-11-26 07:53:19 +0900

    vue.jsのreadyを使うようにした

--- www/js/booklog-input.js
+++ www/js/booklog-input.js
@@ -11,47 +11,43 @@ var app = {
 
 app.initialize();
 
-$(function () {
+new Vue({
+  el: '#app',
+  data: {
 
-  var viewdata = {
-    
     chartdata: {
 
-    "config": {
-      "title": "シングルページWebアプリケーション",
-      "subTitle": "読み始めてから32日目です。",
-      "type": "line",
-      "useVal": "yes",
-      "useMarker": "arc",
-      "bg": "#fff",
-      "textColor": "#000",
-      "xColor": "rgba(100,100,100,0.3)",
-      "maxY": 483,
-      "minY": 0,
-      "lineWidth": "3",
-      "valYOffset": 10,
-      "markerWidth": 15
-    },
-    "data": [
-      ["日", "3/1", "3/2", "3/3", "3/4", "3/5", "3/6", "3/7"],
-      ["シングル~", 30, 43, 50, 164, 175, 265, 378]
-    ]
+      "config": {
+        "title": "シングルページWebアプリケーション",
+        "subTitle": "読み始めてから32日目です。",
+        "type": "line",
+        "useVal": "yes",
+        "useMarker": "arc",
+        "bg": "#fff",
+        "textColor": "#000",
+        "xColor": "rgba(100,100,100,0.3)",
+        "maxY": 483,
+        "minY": 0,
+        "lineWidth": "3",
+        "valYOffset": 10,
+        "markerWidth": 15
+      },
+      "data": [
+        ["日", "3/1", "3/2", "3/3", "3/4", "3/5", "3/6", "3/7"],
+        ["シングル~", 30, 43, 50, 164, 175, 265, 378]
+      ]
+    }
+  },
+
+  ready: function () {
+    ccchart.init('hoge', this.chartdata);
+
+    $("#datepicker").datepicker();
+  },
+
+  methods: {
+    doRegist: function () {
+      alert('regist')
     }
   }
-
-  ccchart.init('hoge', viewdata.chartdata);
-
-  $("#datepicker").datepicker();
-
-  new Vue({
-    el: '#app',
-    data: viewdata,
-    methods: {
-      doRegist: function () {
-        alert('regist')
-      }
-    }
-  })
-
-
-});
+})
